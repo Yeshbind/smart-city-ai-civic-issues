7fc9c793cbeecfcea5c62559d4889d1d4498008c
Anoop <anoopsankar2005@gmail.com>
2026-01-23 18:10:52 +0530

    Add files via upload

---
+++ ai/train.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# Load pretrained YOLOv8 nano model
+model = YOLO("yolov8n.pt")
+
+# Train
+model.train(
+    data="MainDataset/data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    device=0,      # GPU (RTX 4050)
+    workers=0,     # IMPORTANT on Windows
+    name="civic_v1"
+)
