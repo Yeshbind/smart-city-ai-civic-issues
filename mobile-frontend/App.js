c2e92fdf806d520c3cf6fdfb760130a8c374d8fe
Araham07 <159168767+Araham07@users.noreply.github.com>
2026-01-23 18:20:54 +0530

    Add files via upload

---
+++ mobile-frontend/App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import CameraScreen from './screens/CameraScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
